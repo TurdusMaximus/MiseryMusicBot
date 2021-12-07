76504241287ce1d50539c70377f3c57c873a8ee5
Turdus Maximus <87646121+TurdusMaximus@users.noreply.github.com>
2021-12-07 20:19:40 +0530

    Add files via upload

---
+++ Plugs/ytsearch.py
@@ -0,0 +1,50 @@
+import json
+import logging
+
+from helpers.filters import command
+from pyrogram import Client
+from pyrogram.types import (
+    CallbackQuery,
+    InlineKeyboardButton,
+    InlineKeyboardMarkup,
+    Message,
+)
+from youtube_search import YoutubeSearch
+
+logging.basicConfig(
+    level=logging.DEBUG, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+)
+logger = logging.getLogger(__name__)
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+
+
+@Client.on_message(command(["search", f"yt"]))
+async def ytsearch(_, message: Message):
+    
+    keyboard = InlineKeyboardMarkup(
+        [
+            [
+                InlineKeyboardButton(
+                    "🗑 Close", callback_data="close",
+                )
+            ]
+        ]
+    )
+    
+    try:
+        if len(message.command) < 2:
+            await message.reply_text("/search needs an argument!")
+            return
+        query = message.text.split(None, 1)[1]
+        m = await message.reply_text("Searching....")
+        results = YoutubeSearch(query, max_results=5).to_dict()
+        text = ""
+        for i in range(5):
+            text += f"Title - {results[i]['title']}\n"
+            text += f"Duration - {results[i]['duration']}\n"
+            text += f"Views - {results[i]['views']}\n"
+            text += f"Channel - {results[i]['channel']}\n"
+            text += f"https://youtube.com{results[i]['url_suffix']}\n\n"
+        await m.edit(text, disable_web_page_preview=True)
+    except Exception as e:
+        await message.reply_text(str(e))
